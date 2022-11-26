707cf6a14831e201f759846865f9576276d1a97e
Diego Victor <diegovictorgonzaga@gmail.com>
2022-11-26 15:02:34 -0300

    Remove old configuration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { toast, Flip } from 'react-toastify';
 
 import Navigation from './routes';
-
-toast.configure({
-  autoClose: false,
-  transition: Flip,
-  closeOnClick: false,
-});
 
 const root = createRoot(document.getElementById('root'));
 
